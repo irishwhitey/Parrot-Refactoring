229adcb8889dea051f966b963b8e08791789585f
martin <martin@SavilleDomain.Local>
2018-03-15 22:49:52 +0000

    Polymorphism done

--- Parrot/Parrot.cs
+++ Parrot/Parrot.cs
@@ -4,42 +4,84 @@ namespace parrot
 {
     public class Parrot
     {
-        readonly ParrotTypeEnum _type;
-        readonly int _numberOfCoconuts;
-        readonly double _voltage;
-        readonly bool _isNailed;
-        private readonly double _baseSpeed;
-        private double _getLoadFactor = 9.0;
-        private double _minimumBaseSpeedWithVoltage = 24.0;
-        private readonly int _minimumSpeed = 0;
+        protected readonly int _numberOfCoconuts;
+        protected readonly double _voltage;
+        protected readonly bool _isNailed;
+        protected readonly double _baseSpeed;
+        protected double _getLoadFactor = 9.0;
+        protected double _minimumBaseSpeedWithVoltage = 24.0;
+        protected readonly int _minimumSpeed = 0;
 
-        public Parrot(ParrotTypeEnum type, int numberOfCoconuts, double voltage, bool isNailed, double baseSpeed = 12.0)
+        public Parrot(int numberOfCoconuts, double voltage, bool isNailed, double baseSpeed)
         {
-            _type = type;
             _numberOfCoconuts = numberOfCoconuts;
             _voltage = voltage;
             _isNailed = isNailed;
             _baseSpeed = baseSpeed;
         }
 
-        public double GetSpeed()
+        public virtual double GetSpeed()
         {
-            switch (_type)
+            throw new Exception("Should be unreachable");
+        }
+
+        public static Parrot Create(ParrotTypeEnum type, int numberOfCoconuts, double voltage, bool isNailed, double baseSpeed)
+        {
+            switch (type)
             {
                 case ParrotTypeEnum.EUROPEAN:
-                    return _baseSpeed;
+                    return new EuropeanParrot(numberOfCoconuts, voltage, isNailed, baseSpeed);
                 case ParrotTypeEnum.AFRICAN:
-                    return Math.Max(_minimumSpeed, _baseSpeed - _getLoadFactor * _numberOfCoconuts);
+                    return new AfricanParrot(numberOfCoconuts, voltage, isNailed, baseSpeed);
                 case ParrotTypeEnum.NORWEGIAN_BLUE:
-                    return (_isNailed) ? _minimumSpeed : GetBaseSpeedForVoltage(_voltage);
+                    return new NorweigenBlue(numberOfCoconuts, voltage, isNailed, baseSpeed);
             }
+            return new Parrot(numberOfCoconuts, voltage, isNailed, baseSpeed);
+        }
+    }
 
-            throw new Exception("Should be unreachable");
+    public class EuropeanParrot :Parrot
+    {
+        public EuropeanParrot(int numberOfCoconuts, double voltage, bool isNailed, double baseSpeed) 
+            : base(numberOfCoconuts, voltage, isNailed, baseSpeed)
+        {
+        }
+
+        public override double GetSpeed()
+        {
+            return _baseSpeed;
+        }
+    }
+
+    public class AfricanParrot : Parrot
+    {
+        public AfricanParrot(int numberOfCoconuts, double voltage, bool isNailed, double baseSpeed) : base(numberOfCoconuts, voltage, isNailed, baseSpeed)
+        {
+        }
+
+
+        public override double GetSpeed()
+        {
+            return Math.Max(_minimumSpeed, _baseSpeed - _getLoadFactor * _numberOfCoconuts);
+        }
+        
+    }
+
+    public class NorweigenBlue :Parrot
+    {
+        public NorweigenBlue(int numberOfCoconuts, double voltage, bool isNailed, double baseSpeed) : base(numberOfCoconuts, voltage, isNailed, baseSpeed)
+        {
+        }
+
+        public override double GetSpeed()
+        {
+            return (_isNailed) ? _minimumSpeed : GetBaseSpeedForVoltage(_voltage);
         }
 
         private double GetBaseSpeedForVoltage(double voltage)
         {
             return Math.Min(_minimumBaseSpeedWithVoltage, voltage * _baseSpeed);
         }
+
     }
 }
--- Parrot/ParrotTest.cs
+++ Parrot/ParrotTest.cs
@@ -6,26 +6,35 @@ namespace parrot
     [TestFixture]
     public class ParrotTest
     {
+
+        private Parrot Create(ParrotTypeEnum type, int numberOfCoconuts, double voltage, bool isNailed, double baseSpeed)
+        {
+            return Parrot.Create( type, numberOfCoconuts, voltage, isNailed, baseSpeed);
+        }
+
         private double _baseSpeed = 12.0;
         [Test]
         public void GetSpeedOfEuropeanParrot()
         {
             var expectedBaseSpeed = _baseSpeed;
-            Parrot parrot = new Parrot(ParrotTypeEnum.EUROPEAN, 0, 0, false, _baseSpeed);
+            var numberOfCoconuts = 0;
+            var voltage = 0;
+            var isNailed = false;
+            Parrot parrot = Create(ParrotTypeEnum.EUROPEAN, numberOfCoconuts, voltage, isNailed, _baseSpeed);
             Assert.AreEqual(expectedBaseSpeed, parrot.GetSpeed());
         }
 
         [Test]
         public void GetSpeedOfAfricanParrot_With_One_Coconut()
         {
-            Parrot parrot = new Parrot(ParrotTypeEnum.AFRICAN, 1, 0, false, _baseSpeed);
+            Parrot parrot = Create(ParrotTypeEnum.AFRICAN, 1, 0, false, _baseSpeed);
             Assert.AreEqual(3.0, parrot.GetSpeed());
         }
 
         [Test]
         public void GetSpeedOfAfricanParrot_With_Two_Coconuts()
         {
-            Parrot parrot = new Parrot(ParrotTypeEnum.AFRICAN, 2, 0, false, _baseSpeed);
+            Parrot parrot = Create(ParrotTypeEnum.AFRICAN, 2, 0, false, _baseSpeed);
             Assert.AreEqual(0.0, parrot.GetSpeed());
         }
 
@@ -34,28 +43,29 @@ namespace parrot
         {
             var baseSpeed = 12.0;
             var expected = baseSpeed;
-            Parrot parrot = new Parrot(ParrotTypeEnum.AFRICAN, 0, 0, false, baseSpeed);
+            Parrot parrot = Create(ParrotTypeEnum.AFRICAN, 0, 0, false, baseSpeed);
             Assert.AreEqual(expected, parrot.GetSpeed());
         }
 
         [Test]
         public void GetSpeedNorwegianBlueParrot_nailed()
         {
-            Parrot parrot = new Parrot(ParrotTypeEnum.NORWEGIAN_BLUE, 0, 0, true, _baseSpeed);
+            Parrot parrot = Create(ParrotTypeEnum.NORWEGIAN_BLUE, 0, 0, true, _baseSpeed);
             Assert.AreEqual(0.0, parrot.GetSpeed());
         }
 
         [Test]
         public void GetSpeedNorwegianBlueParrot_not_nailed()
         {
-            Parrot parrot = new Parrot(ParrotTypeEnum.NORWEGIAN_BLUE, 0, 1.5, false, _baseSpeed);
-            Assert.AreEqual(18.0, parrot.GetSpeed());
+            var voltage = 1.5;
+            Parrot parrot = Create(ParrotTypeEnum.NORWEGIAN_BLUE, 0, voltage, false, _baseSpeed);
+            Assert.AreEqual(_baseSpeed * voltage, parrot.GetSpeed());
         }
 
         [Test]
         public void GetSpeedNorwegianBlueParrot_not_nailed_high_voltage()
         {
-            Parrot parrot = new Parrot(ParrotTypeEnum.NORWEGIAN_BLUE, 0, 4, false, _baseSpeed);
+            Parrot parrot = Create(ParrotTypeEnum.NORWEGIAN_BLUE, 0, 4, false, _baseSpeed);
             Assert.AreEqual(24.0, parrot.GetSpeed());
         }
     }
