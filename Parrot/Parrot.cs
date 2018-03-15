01183f444b8f0dbb771f62feb30a027fbd0ddd90
martin <martin@SavilleDomain.Local>
2018-03-15 22:20:47 +0000

    Refactored to reduce duplication

---
+++ Parrot/Parrot.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace parrot
+{
+    public class Parrot
+    {
+        readonly ParrotTypeEnum _type;
+        readonly int _numberOfCoconuts;
+        readonly double _voltage;
+        readonly bool _isNailed;
+        private readonly double _baseSpeed;
+        private double _getLoadFactor = 9.0;
+        private double _minimumBaseSpeedWithVoltage = 24.0;
+        private readonly int _minimumSpeed = 0;
+
+        public Parrot(ParrotTypeEnum type, int numberOfCoconuts, double voltage, bool isNailed, double baseSpeed = 12.0)
+        {
+            _type = type;
+            _numberOfCoconuts = numberOfCoconuts;
+            _voltage = voltage;
+            _isNailed = isNailed;
+            _baseSpeed = baseSpeed;
+        }
+
+        public double GetSpeed()
+        {
+            switch (_type)
+            {
+                case ParrotTypeEnum.EUROPEAN:
+                    return _baseSpeed;
+                case ParrotTypeEnum.AFRICAN:
+                    return Math.Max(_minimumSpeed, _baseSpeed - _getLoadFactor * _numberOfCoconuts);
+                case ParrotTypeEnum.NORWEGIAN_BLUE:
+                    return (_isNailed) ? _minimumSpeed : GetBaseSpeedForVoltage(_voltage);
+            }
+
+            throw new Exception("Should be unreachable");
+        }
+
+        private double GetBaseSpeedForVoltage(double voltage)
+        {
+            return Math.Min(_minimumBaseSpeedWithVoltage, voltage * _baseSpeed);
+        }
+    }
+}
---
+++ Parrot/ParrotTest.cs
@@ -0,0 +1,62 @@
+﻿
+using NUnit.Framework;
+
+namespace parrot
+{
+    [TestFixture]
+    public class ParrotTest
+    {
+        private double _baseSpeed = 12.0;
+        [Test]
+        public void GetSpeedOfEuropeanParrot()
+        {
+            var expectedBaseSpeed = _baseSpeed;
+            Parrot parrot = new Parrot(ParrotTypeEnum.EUROPEAN, 0, 0, false, _baseSpeed);
+            Assert.AreEqual(expectedBaseSpeed, parrot.GetSpeed());
+        }
+
+        [Test]
+        public void GetSpeedOfAfricanParrot_With_One_Coconut()
+        {
+            Parrot parrot = new Parrot(ParrotTypeEnum.AFRICAN, 1, 0, false, _baseSpeed);
+            Assert.AreEqual(3.0, parrot.GetSpeed());
+        }
+
+        [Test]
+        public void GetSpeedOfAfricanParrot_With_Two_Coconuts()
+        {
+            Parrot parrot = new Parrot(ParrotTypeEnum.AFRICAN, 2, 0, false, _baseSpeed);
+            Assert.AreEqual(0.0, parrot.GetSpeed());
+        }
+
+        [Test]
+        public void GetSpeedOfAfricanParrot_With_No_Coconuts()
+        {
+            var baseSpeed = 12.0;
+            var expected = baseSpeed;
+            Parrot parrot = new Parrot(ParrotTypeEnum.AFRICAN, 0, 0, false, baseSpeed);
+            Assert.AreEqual(expected, parrot.GetSpeed());
+        }
+
+        [Test]
+        public void GetSpeedNorwegianBlueParrot_nailed()
+        {
+            Parrot parrot = new Parrot(ParrotTypeEnum.NORWEGIAN_BLUE, 0, 0, true, _baseSpeed);
+            Assert.AreEqual(0.0, parrot.GetSpeed());
+        }
+
+        [Test]
+        public void GetSpeedNorwegianBlueParrot_not_nailed()
+        {
+            Parrot parrot = new Parrot(ParrotTypeEnum.NORWEGIAN_BLUE, 0, 1.5, false, _baseSpeed);
+            Assert.AreEqual(18.0, parrot.GetSpeed());
+        }
+
+        [Test]
+        public void GetSpeedNorwegianBlueParrot_not_nailed_high_voltage()
+        {
+            Parrot parrot = new Parrot(ParrotTypeEnum.NORWEGIAN_BLUE, 0, 4, false, _baseSpeed);
+            Assert.AreEqual(24.0, parrot.GetSpeed());
+        }
+    }
+}
